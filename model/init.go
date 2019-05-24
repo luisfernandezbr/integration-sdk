0621c40c8808814198213f3bededfbfb3c70fd1a
Jeff Haynie <jhaynie@pinpoint.com>
2019-05-24 20:06:13 +0000

    autogenerated updates for schema version 0.0.43

--- model/init.go
+++ model/init.go
@@ -15,26 +15,8 @@ import (
 // New returns a new instanceof from a ModelNameType
 func New(name datamodel.ModelNameType) datamodel.Model {
 	switch name {
-	case "customer.CostCenter":
-		return new(customer.CostCenter)
-	case "customer.Team":
-		return new(customer.Team)
-	case "customer.User":
-		return new(customer.User)
-	case "sourcecode.CommitFile":
-		return new(sourcecode.CommitFile)
-	case "sourcecode.PullRequest":
-		return new(sourcecode.PullRequest)
-	case "sourcecode.Repo":
-		return new(sourcecode.Repo)
-	case "sourcecode.User":
-		return new(sourcecode.User)
-	case "sourcecode.Branch":
-		return new(sourcecode.Branch)
-	case "sourcecode.Changelog":
-		return new(sourcecode.Changelog)
-	case "sourcecode.Commit":
-		return new(sourcecode.Commit)
+	case "work.Changelog":
+		return new(work.Changelog)
 	case "work.CustomField":
 		return new(work.CustomField)
 	case "work.Issue":
@@ -45,12 +27,30 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 		return new(work.Sprint)
 	case "work.User":
 		return new(work.User)
-	case "work.Changelog":
-		return new(work.Changelog)
-	case "codequality.Project":
-		return new(codequality.Project)
 	case "codequality.Metric":
 		return new(codequality.Metric)
+	case "codequality.Project":
+		return new(codequality.Project)
+	case "customer.CostCenter":
+		return new(customer.CostCenter)
+	case "customer.Team":
+		return new(customer.Team)
+	case "customer.User":
+		return new(customer.User)
+	case "sourcecode.Changelog":
+		return new(sourcecode.Changelog)
+	case "sourcecode.Commit":
+		return new(sourcecode.Commit)
+	case "sourcecode.CommitFile":
+		return new(sourcecode.CommitFile)
+	case "sourcecode.PullRequest":
+		return new(sourcecode.PullRequest)
+	case "sourcecode.Repo":
+		return new(sourcecode.Repo)
+	case "sourcecode.User":
+		return new(sourcecode.User)
+	case "sourcecode.Branch":
+		return new(sourcecode.Branch)
 	}
 	panic("invalid type specific: " + name)
 }
@@ -58,16 +58,6 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 // NewFromTopic returns a new instanceof from a TopicNameType
 func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 	switch name {
-	case "codequality_Metric_topic":
-		return new(codequality.Metric)
-	case "codequality_Project_topic":
-		return new(codequality.Project)
-	case "customer_CostCenter_topic":
-		return new(customer.CostCenter)
-	case "customer_Team_topic":
-		return new(customer.Team)
-	case "customer_User_topic":
-		return new(customer.User)
 	case "sourcecode_Commit_topic":
 		return new(sourcecode.Commit)
 	case "sourcecode_CommitFile_topic":
@@ -94,6 +84,16 @@ func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 		return new(work.Sprint)
 	case "work_User_topic":
 		return new(work.User)
+	case "codequality_Metric_topic":
+		return new(codequality.Metric)
+	case "codequality_Project_topic":
+		return new(codequality.Project)
+	case "customer_CostCenter_topic":
+		return new(customer.CostCenter)
+	case "customer_Team_topic":
+		return new(customer.Team)
+	case "customer_User_topic":
+		return new(customer.User)
 	}
 	panic("invalid type specific: " + name)
 }
@@ -101,15 +101,15 @@ func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 // GetMaterializedTopics returns an array of topics to be materialized
 func GetMaterializedTopics() []datamodel.TopicNameType {
 	return []datamodel.TopicNameType{
-		datamodel.TopicNameType(customer_CostCenter_topic),
-		datamodel.TopicNameType(customer_Team_topic),
-		datamodel.TopicNameType(customer_User_topic),
-		datamodel.TopicNameType(sourcecode_CommitFile_topic),
-		datamodel.TopicNameType(sourcecode_PullRequest_topic),
-		datamodel.TopicNameType(sourcecode_Repo_topic),
 		datamodel.TopicNameType(sourcecode_Branch_topic),
 		datamodel.TopicNameType(sourcecode_Changelog_topic),
 		datamodel.TopicNameType(sourcecode_Commit_topic),
+		datamodel.TopicNameType(sourcecode_CommitFile_topic),
+		datamodel.TopicNameType(sourcecode_PullRequest_topic),
+		datamodel.TopicNameType(sourcecode_Repo_topic),
 		datamodel.TopicNameType(work_Project_topic),
+		datamodel.TopicNameType(customer_CostCenter_topic),
+		datamodel.TopicNameType(customer_Team_topic),
+		datamodel.TopicNameType(customer_User_topic),
 	}
 }
