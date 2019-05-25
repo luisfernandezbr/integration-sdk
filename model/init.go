9d40773b8b2a238c5beabea41f4b142cbcf6ddf4
Jeff Haynie <jhaynie@pinpoint.com>
2019-05-25 00:23:57 +0000

    autogenerated updates for schema version 0.0.47

--- model/init.go
+++ model/init.go
@@ -47,10 +47,10 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 		return new(work.User)
 	case "work.Changelog":
 		return new(work.Changelog)
-	case "codequality.Project":
-		return new(codequality.Project)
 	case "codequality.Metric":
 		return new(codequality.Metric)
+	case "codequality.Project":
+		return new(codequality.Project)
 	}
 	panic("invalid type specific: " + name)
 }
@@ -58,16 +58,16 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 // NewFromTopic returns a new instanceof from a TopicNameType
 func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 	switch name {
+	case "codequality_Metric_topic":
+		return new(codequality.Metric)
+	case "codequality_Project_topic":
+		return new(codequality.Project)
 	case "customer_CostCenter_topic":
 		return new(customer.CostCenter)
 	case "customer_Team_topic":
 		return new(customer.Team)
 	case "customer_User_topic":
 		return new(customer.User)
-	case "sourcecode_PullRequest_topic":
-		return new(sourcecode.PullRequest)
-	case "sourcecode_Repo_topic":
-		return new(sourcecode.Repo)
 	case "sourcecode_User_topic":
 		return new(sourcecode.User)
 	case "sourcecode_Branch_topic":
@@ -80,8 +80,10 @@ func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 		return new(sourcecode.CommitActivity)
 	case "sourcecode_CommitFile_topic":
 		return new(sourcecode.CommitFile)
-	case "work_Changelog_topic":
-		return new(work.Changelog)
+	case "sourcecode_PullRequest_topic":
+		return new(sourcecode.PullRequest)
+	case "sourcecode_Repo_topic":
+		return new(sourcecode.Repo)
 	case "work_CustomField_topic":
 		return new(work.CustomField)
 	case "work_Issue_topic":
@@ -92,10 +94,8 @@ func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 		return new(work.Sprint)
 	case "work_User_topic":
 		return new(work.User)
-	case "codequality_Project_topic":
-		return new(codequality.Project)
-	case "codequality_Metric_topic":
-		return new(codequality.Metric)
+	case "work_Changelog_topic":
+		return new(work.Changelog)
 	}
 	panic("invalid type specific: " + name)
 }
@@ -106,14 +106,14 @@ func GetMaterializedTopics() []datamodel.TopicNameType {
 		datamodel.TopicNameType("customer_CostCenter_topic"),
 		datamodel.TopicNameType("customer_Team_topic"),
 		datamodel.TopicNameType("customer_User_topic"),
+		datamodel.TopicNameType("sourcecode_Repo_topic"),
+		datamodel.TopicNameType("sourcecode_User_topic"),
 		datamodel.TopicNameType("sourcecode_Branch_topic"),
 		datamodel.TopicNameType("sourcecode_Changelog_topic"),
 		datamodel.TopicNameType("sourcecode_Commit_topic"),
 		datamodel.TopicNameType("sourcecode_CommitActivity_topic"),
 		datamodel.TopicNameType("sourcecode_CommitFile_topic"),
 		datamodel.TopicNameType("sourcecode_PullRequest_topic"),
-		datamodel.TopicNameType("sourcecode_Repo_topic"),
-		datamodel.TopicNameType("sourcecode_User_topic"),
 		datamodel.TopicNameType("work_Project_topic"),
 	}
 }
