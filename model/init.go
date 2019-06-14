c39139c741b9a5d16ae9aea1d268414933a4d7bd
Jeff Haynie <jhaynie@pinpoint.com>
2019-06-14 18:25:55 +0000

    autogenerated updates for schema version 0.0.128

--- model/init.go
+++ model/init.go
@@ -13,12 +13,18 @@ import (
 // New returns a new instanceof from a ModelNameType
 func New(name datamodel.ModelNameType) datamodel.Model {
 	switch name {
+	case "codequality.Metric":
+		return new(codequality.Metric)
+	case "codequality.Project":
+		return new(codequality.Project)
+	case "customer.User":
+		return new(customer.User)
 	case "customer.CostCenter":
 		return new(customer.CostCenter)
 	case "customer.Team":
 		return new(customer.Team)
-	case "customer.User":
-		return new(customer.User)
+	case "sourcecode.User":
+		return new(sourcecode.User)
 	case "sourcecode.Blame":
 		return new(sourcecode.Blame)
 	case "sourcecode.Branch":
@@ -31,8 +37,10 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 		return new(sourcecode.PullRequest)
 	case "sourcecode.Repo":
 		return new(sourcecode.Repo)
-	case "sourcecode.User":
-		return new(sourcecode.User)
+	case "work.Issue":
+		return new(work.Issue)
+	case "work.Project":
+		return new(work.Project)
 	case "work.Sprint":
 		return new(work.Sprint)
 	case "work.User":
@@ -41,14 +49,6 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 		return new(work.Changelog)
 	case "work.CustomField":
 		return new(work.CustomField)
-	case "work.Issue":
-		return new(work.Issue)
-	case "work.Project":
-		return new(work.Project)
-	case "codequality.Metric":
-		return new(codequality.Metric)
-	case "codequality.Project":
-		return new(codequality.Project)
 	}
 	panic("invalid type specific: " + name)
 }
@@ -56,12 +56,20 @@ func New(name datamodel.ModelNameType) datamodel.Model {
 // NewFromTopic returns a new instanceof from a TopicNameType
 func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 	switch name {
-	case "customer_Team_topic":
-		return new(customer.Team)
+	case "codequality_Metric_topic":
+		return new(codequality.Metric)
+	case "codequality_Project_topic":
+		return new(codequality.Project)
 	case "customer_User_topic":
 		return new(customer.User)
 	case "customer_CostCenter_topic":
 		return new(customer.CostCenter)
+	case "customer_Team_topic":
+		return new(customer.Team)
+	case "sourcecode_CommitFile_topic":
+		return new(sourcecode.CommitFile)
+	case "sourcecode_PullRequest_topic":
+		return new(sourcecode.PullRequest)
 	case "sourcecode_Repo_topic":
 		return new(sourcecode.Repo)
 	case "sourcecode_User_topic":
@@ -72,12 +80,6 @@ func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 		return new(sourcecode.Branch)
 	case "sourcecode_Commit_topic":
 		return new(sourcecode.Commit)
-	case "sourcecode_CommitFile_topic":
-		return new(sourcecode.CommitFile)
-	case "sourcecode_PullRequest_topic":
-		return new(sourcecode.PullRequest)
-	case "work_Project_topic":
-		return new(work.Project)
 	case "work_Sprint_topic":
 		return new(work.Sprint)
 	case "work_User_topic":
@@ -88,10 +90,8 @@ func NewFromTopic(name datamodel.TopicNameType) datamodel.Model {
 		return new(work.CustomField)
 	case "work_Issue_topic":
 		return new(work.Issue)
-	case "codequality_Metric_topic":
-		return new(codequality.Metric)
-	case "codequality_Project_topic":
-		return new(codequality.Project)
+	case "work_Project_topic":
+		return new(work.Project)
 	}
 	panic("invalid type specific: " + name)
 }
@@ -114,47 +114,47 @@ func GetMaterializedTopics() []datamodel.TopicNameType {
 // GetTopics returns an array of topics that are configured
 func GetTopics() []datamodel.TopicNameType {
 	return []datamodel.TopicNameType{
+		datamodel.TopicNameType("codequality_Metric_topic"),
+		datamodel.TopicNameType("codequality_Project_topic"),
 		datamodel.TopicNameType("customer_CostCenter_topic"),
-		datamodel.TopicNameType("sourcecode_Blame_topic"),
 		datamodel.TopicNameType("customer_Team_topic"),
 		datamodel.TopicNameType("customer_User_topic"),
+		datamodel.TopicNameType("sourcecode_Blame_topic"),
 		datamodel.TopicNameType("sourcecode_Branch_topic"),
 		datamodel.TopicNameType("sourcecode_Commit_topic"),
 		datamodel.TopicNameType("sourcecode_CommitFile_topic"),
 		datamodel.TopicNameType("sourcecode_PullRequest_topic"),
 		datamodel.TopicNameType("sourcecode_Repo_topic"),
 		datamodel.TopicNameType("sourcecode_User_topic"),
-		datamodel.TopicNameType("work_CustomField_topic"),
-		datamodel.TopicNameType("work_Issue_topic"),
 		datamodel.TopicNameType("work_Project_topic"),
 		datamodel.TopicNameType("work_Sprint_topic"),
 		datamodel.TopicNameType("work_User_topic"),
 		datamodel.TopicNameType("work_Changelog_topic"),
-		datamodel.TopicNameType("codequality_Metric_topic"),
-		datamodel.TopicNameType("codequality_Project_topic"),
+		datamodel.TopicNameType("work_CustomField_topic"),
+		datamodel.TopicNameType("work_Issue_topic"),
 	}
 }
 
 // GetModelNames returns an array of model names that are configured
 func GetModelNames() []datamodel.ModelNameType {
 	return []datamodel.ModelNameType{
+		datamodel.ModelNameType("codequality_Metric"),
+		datamodel.ModelNameType("codequality_Project"),
 		datamodel.ModelNameType("customer_CostCenter"),
-		datamodel.ModelNameType("sourcecode_Blame"),
 		datamodel.ModelNameType("customer_Team"),
 		datamodel.ModelNameType("customer_User"),
+		datamodel.ModelNameType("sourcecode_Blame"),
 		datamodel.ModelNameType("sourcecode_Branch"),
 		datamodel.ModelNameType("sourcecode_Commit"),
 		datamodel.ModelNameType("sourcecode_CommitFile"),
 		datamodel.ModelNameType("sourcecode_PullRequest"),
 		datamodel.ModelNameType("sourcecode_Repo"),
 		datamodel.ModelNameType("sourcecode_User"),
-		datamodel.ModelNameType("work_CustomField"),
-		datamodel.ModelNameType("work_Issue"),
 		datamodel.ModelNameType("work_Project"),
 		datamodel.ModelNameType("work_Sprint"),
 		datamodel.ModelNameType("work_User"),
 		datamodel.ModelNameType("work_Changelog"),
-		datamodel.ModelNameType("codequality_Metric"),
-		datamodel.ModelNameType("codequality_Project"),
+		datamodel.ModelNameType("work_CustomField"),
+		datamodel.ModelNameType("work_Issue"),
 	}
 }
