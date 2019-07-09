5bbef1ddc891a41aafb8b22e227477f472bf9138
Jeff Haynie <jhaynie@pinpoint.com>
2019-07-09 07:37:14 +0000

    autogenerated updates for schema version 0.0.259

--- agent/export_request.go
+++ agent/export_request.go
@@ -699,7 +699,7 @@ func GetExportRequestAvroSchemaSpec() string {
 			},
 			map[string]interface{}{
 				"name": "date",
-				"type": map[string]interface{}{"type": "record", "name": "date", "fields": []interface{}{map[string]interface{}{"type": "long", "name": "epoch", "doc": "the date in epoch format"}, map[string]interface{}{"type": "long", "name": "offset", "doc": "the timezone offset from GMT"}, map[string]interface{}{"doc": "the date in RFC3339 format", "type": "string", "name": "rfc3339"}}, "doc": "the date when the request was made"},
+				"type": map[string]interface{}{"type": "record", "name": "date", "fields": []interface{}{map[string]interface{}{"type": "long", "name": "epoch", "doc": "the date in epoch format"}, map[string]interface{}{"name": "offset", "doc": "the timezone offset from GMT", "type": "long"}, map[string]interface{}{"type": "string", "name": "rfc3339", "doc": "the date in RFC3339 format"}}, "doc": "the date when the request was made"},
 			},
 			map[string]interface{}{
 				"name": "id",
@@ -707,7 +707,7 @@ func GetExportRequestAvroSchemaSpec() string {
 			},
 			map[string]interface{}{
 				"name": "integrations",
-				"type": map[string]interface{}{"type": "array", "name": "integrations", "items": map[string]interface{}{"type": "record", "name": "integrations", "fields": []interface{}{}, "doc": "The integrations that should be exported and their current configuration"}},
+				"type": map[string]interface{}{"items": map[string]interface{}{"type": "record", "name": "integrations", "fields": []interface{}{}, "doc": "The integrations that should be exported and their current configuration"}, "type": "array", "name": "integrations"},
 			},
 			map[string]interface{}{
 				"name": "ref_id",
